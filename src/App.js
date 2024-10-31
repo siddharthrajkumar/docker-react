bf99908665a436ed7e4e57206dd1e6eb9e1365e9
siddharthrajkumar <sidd9695r@gmail.com>
2024-11-01 01:18:13 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Gomu Gomu Noooooo000!
+          Gomu Gomu Noooooo!
         </a>
       </header>
     </div>
